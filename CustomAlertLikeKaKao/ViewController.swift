c168878ceb238a304c6a69b0eec65684bf91c3e1
InKwon James Kim <devikkim@gmail.com>
2019-07-02 18:19:38 +0900

    Initial Commit

---
+++ CustomAlertLikeKaKao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAlertLikeKaKao
+//
+//  Created by InKwon Devik Kim on 02/07/2019.
+//  Copyright © 2019 InKwon Devik Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
